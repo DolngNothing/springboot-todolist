06098a6280dd8c177463bb9833fe1399a41cdfa9
DolngNothing <1178363215@qq.com>
2020-08-06 09:29:47 +0800

    [serviceTest][christopher] feature: implement should_return_new_todo_when_update_given_new_todo_todo_id

--- src/test/java/com/thoughtworks/todoList/service/TodoServiceTest.java
+++ src/test/java/com/thoughtworks/todoList/service/TodoServiceTest.java
@@ -2,6 +2,8 @@ package com.thoughtworks.todoList.service;
 
 import com.thoughtworks.todoList.dto.TodoRequest;
 import com.thoughtworks.todoList.dto.TodoResponse;
+import com.thoughtworks.todoList.exception.IllegalOperationException;
+import com.thoughtworks.todoList.exception.NoSuchDataException;
 import com.thoughtworks.todoList.mapper.TodoMapper;
 import com.thoughtworks.todoList.model.Todo;
 import com.thoughtworks.todoList.repository.TodoRepository;
@@ -52,16 +54,17 @@ public class TodoServiceTest {
     }
 
     @Test
-    void should_return_new_todo_when_update_given_new_todo_todo_id() {
+    void should_return_new_todo_when_update_given_new_todo_todo_id() throws IllegalOperationException, NoSuchDataException {
         //given
         TodoRepository todoRepository = Mockito.mock(TodoRepository.class);
         TodoService todoService = new TodoService(todoRepository);
-        Todo todo=new Todo(1,"todo",false);
-        given(todoRepository.findById(anyInt())).willReturn(Optional.of(todo));
+        TodoRequest todoRequest=new TodoRequest(1,"todo",false);
+        given(todoRepository.findById(anyInt())).willReturn(Optional.of(TodoMapper.map(todoRequest)));
+        given(todoRepository.save(any(Todo.class))).willReturn(TodoMapper.map(todoRequest));
         //when
-        TodoResponse newTodo = todoService.update(1,todo);
+        TodoResponse newTodo = todoService.update(1,todoRequest);
 
         //then
-        assertEquals(TodoMapper.map(todo), newTodo);
+        assertEquals(TodoMapper.map(TodoMapper.map(todoRequest)), newTodo);
     }
 }
