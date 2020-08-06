aa6e86e7166f88a2264d068b08f704c5b3d1e3a0
DolngNothing <1178363215@qq.com>
2020-08-06 10:53:06 +0800

    [integration][christopher] feature: implement should_return_new_todo_when_update_given_id_new_todo

--- src/main/java/com/thoughtworks/todoList/controller/TodoController.java
+++ src/main/java/com/thoughtworks/todoList/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.thoughtworks.todoList.controller;
 
 import com.thoughtworks.todoList.dto.TodoRequest;
 import com.thoughtworks.todoList.dto.TodoResponse;
+import com.thoughtworks.todoList.exception.IllegalOperationException;
+import com.thoughtworks.todoList.exception.NoSuchDataException;
 import com.thoughtworks.todoList.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,5 +32,9 @@ public class TodoController {
         todoService.deleteById(id);
     }
 
+    @PutMapping("/{id}")
+    public TodoResponse update(@PathVariable Integer id,@RequestBody TodoRequest todoRequest) throws IllegalOperationException, NoSuchDataException {
+        return todoService.update(id,todoRequest);
+    }
 
 }
